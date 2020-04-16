a8d9a7af5180920e1310b3343302a2b8d5546fd6
Spyro119 <samueljubinville119@gmail.com>
2020-04-16 11:26:26 -0400

    new pictures --again

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -5,7 +5,7 @@ class Employee < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   def password_complexity
     # Regexp extracted from https://stackoverflow.com/questions/19605150/regex-for-password-must-contain-at-least-eight-characters-at-least-one-number-a
-    return if password.blank? #|| password =~ /(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-])/
+    return if password.blank? || password =~ /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/
 
     errors.add :password, ' must be at least 8 characters long.'
   end
